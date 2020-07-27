ad0d26cf93255b965a631030895746b150d20b75
hyeonkimmm <43769123+hyeonkimmm@users.noreply.github.com>
2020-07-27 10:51:40 +0900

    Create 전기버스.py

---
+++ Intermediate/전기버스.py
@@ -0,0 +1,21 @@
+for i in range(int(input())):
+  K,N,M = map(int,input().split())
+  # K 이동할 수 있는 정류장 수
+  # N 종점 정류장
+  # M 정류장 개수
+  bus = list(map(int,input().split()))
+  start = 0
+  cnt =0
+  end = K
+  while True:
+    if start+K >=N:
+      break
+    for a in range(K,0,-1):
+      if start+a in bus:
+        start +=a
+        cnt+=1
+        break
+    else:
+      cnt=0
+      break
+  print("#{} {}".format(i+1,cnt))
